a6ec8575f6a2393ee51db9cd613e7d81988063e9
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-05-02 09:44:16 -0300

    Criando o index

--- agenda-telefonica/src/index.js
+++ agenda-telefonica/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ToastContainer, Flip } from 'react-toastify'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import AppProvider from './hooks'
+import Routes from './routes/routes'
+import GlobalStyles from './styles/globalStyles'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    {/* Disponibilizando context */}
+    <AppProvider>
+      {/* Rotas */}
+      <Routes />
+    </AppProvider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {/* Configuração do TOAST */}
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      transition={Flip}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+    {/* Estilos globais */}
+    <GlobalStyles />
+  </>
+)
