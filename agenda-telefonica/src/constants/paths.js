456efc0429918a293b3a6c2e66e0a1a925cca88f
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-05-02 09:45:55 -0300

    Criando links

---
+++ agenda-telefonica/src/constants/paths.js
@@ -0,0 +1,13 @@
+/*
+  Links de navegação
+*/
+
+const paths = {
+  SingIn: '/login',
+  Register: '/cadastro',
+  List: '/',
+  NewCtt: '/novo-contato',
+  Edit: '/editar-contato'
+}
+
+export default paths
---
+++ agenda-telefonica/src/containers/index.js
@@ -0,0 +1,10 @@
+/*
+Exportação de todas pastas do containers
+*/
+
+export * from './Login'
+export * from './Cadastro'
+export * from './ContactList'
+export * from './NewContact'
+export * from './EditContact'
+export * from './Pages'
